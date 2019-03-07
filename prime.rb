dbcdd13a1badef5e3a7e37949cd0d612e79b3e99
Jeff M <jkdmarks@gmail.com>
2019-03-07 16:41:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,14 @@
 def prime?(num)
   output = true
-  if num > 0
-    num_array = (2..num-1).to_a
-  else
-    num_array = []
+  if num < 0
     output = false
-  end
-
-  for i in num_array
-    if num % i == 0
-      output = false
+  elsif num == 1 || num == 2 || num == 3
+    output = false
+  else
+    for i in (2..num-1).to_a
+      if num % i == 0
+        output = false
+      end
     end
   end
   output
