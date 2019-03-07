1f8528d00cfd67c556556a4a8583d39380637ed3
Jeff M <jkdmarks@gmail.com>
2019-03-07 16:41:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(num)
   output = true
-  if num < 0
+  if num <= 1
     output = false
-  elsif num == 1 || num == 2 || num == 3
-    output = false
+  elsif num == 2 || num == 3
+    output = true
   else
     for i in (2..num-1).to_a
       if num % i == 0
